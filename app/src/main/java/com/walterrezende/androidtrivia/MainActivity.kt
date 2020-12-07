4afb2b00e08f2e0100ca69cefd4adc222274838e
walzero <walter-zero@hotmail.com>
2020-12-07 00:14:13 -0300

    -Removed NavigationCompatActivity -Added Drawer for extra menus -Refactored Code

--- app/src/main/java/com/walterrezende/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/walterrezende/androidtrivia/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.walterrezende.androidtrivia
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.walterrezende.androidtrivia.databinding.ActivityMainBinding
+import com.walterrezende.androidtrivia.extensions.bindActionBarToNavigation
 
-class MainActivity : NavigationCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override val navHostFragmentId: Int = R.id.myNavHostFragment
+    private val navHostFragmentId: Int = R.id.myNavHostFragment
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        bindActionBarToNavigation(navHostFragmentId, binding.drawerLayout, binding.navView)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(navHostFragmentId)
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
     }
 }
--- app/src/main/java/com/walterrezende/androidtrivia/extensions/NavigationExtensions.kt
+++ app/src/main/java/com/walterrezende/androidtrivia/extensions/NavigationExtensions.kt
@@ -3,11 +3,13 @@ package com.walterrezende.androidtrivia.extensions
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 fun View.setOnClickNavigation(navDirection: NavDirections) {
     setOnClickListener { it?.findNavController()?.navigate(navDirection) }
@@ -20,4 +22,14 @@ fun Fragment.navigateTo(navDirection: NavDirections) {
 fun AppCompatActivity.bindActionBarToNavigation(@IdRes navControllerId: Int) {
     val navController = findNavController(navControllerId)
     NavigationUI.setupActionBarWithNavController(this, navController)
+}
+
+fun AppCompatActivity.bindActionBarToNavigation(
+    @IdRes navControllerId: Int,
+    drawer: DrawerLayout,
+    navView: NavigationView
+) {
+    val navController = findNavController(navControllerId)
+    NavigationUI.setupActionBarWithNavController(this, navController, drawer)
+    NavigationUI.setupWithNavController(navView, navController)
 }
