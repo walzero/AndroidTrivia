c432c286984c9f0d4044d3031655651310a8d1f0
walzero <walter-zero@hotmail.com>
2020-12-07 00:48:57 -0300

    -Added animarions -Locked drawer outside home fragment

--- app/src/main/java/com/walterrezende/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/walterrezende/androidtrivia/MainActivity.kt
@@ -3,6 +3,9 @@ package com.walterrezende.androidtrivia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.walterrezende.androidtrivia.databinding.ActivityMainBinding
@@ -17,6 +20,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         bindActionBarToNavigation(navHostFragmentId, binding.drawerLayout, binding.navView)
+        val navController = findNavController(navHostFragmentId)
+        navController.addOnDestinationChangedListener { nc, nd, args -> validateDrawerMenu(nc, nd) }
+    }
+
+    private fun validateDrawerMenu(nc: NavController, nd: NavDestination) {
+        val lockMode = when(nd.id) {
+            nc.graph.startDestination -> DrawerLayout.LOCK_MODE_UNLOCKED
+            else -> DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+        }
+
+        binding.drawerLayout.setDrawerLockMode(lockMode)
     }
 
     override fun onSupportNavigateUp(): Boolean {
